8add8d997139b76ba2876e0ed970c289998933bf
ranula.gamage <ranula.g@insighture.com>
2023-10-18 21:43:13 +0530

    paw features added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,39 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules:['@vite-pwa/nuxt'],
+  pwa:{
+    manifest:{
+      name:"Nuxt3 PWA",
+      short_name:"Nuxt3 PWA",
+      description:"Nuxt 3 PWA ",
+      icons:[
+        {
+          src: '/icons/application-icon-144x144.png',
+          size: "144x144",
+          type: 'image/png',
+          purpose: 'any'
+        }
+      ],
+      screenshots:[
+        {
+          src: "icons/CompressJPEG.online_500x500-image.png",
+          sizes: "500x500",
+          type: "image/jpg",
+          form_factor: "narrow"
+        },
+        {
+          src: "icons/600x400.png",
+          sizes: "600x400",
+          type: "image/jpg",
+          form_factor: "wide"
+        },
+      ]
+    },
+    workbox:{
+      navigateFallback:'/'
+    },
+    devOptions:{
+      enabled:true,
+      type:"module"
+    }
+  },
 })
