01e2c7c84f59702781d7afb56cc3d5c9e2b99a9c
ranula.gamage <ranula.g@insighture.com>
2023-10-19 00:27:36 +0530

    identify is online or not

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,28 @@
       JSON file and store in IndexedDB.">
       Generate and Store Large JSON
     </button>
-    <button class="status">Online</button>
+    <button class="status" :class="{ online: isOnline, offline: !isOnline }">
+      {{ onlineStatus }}
+    </button>
   </nav>
 <!--  <VitePwaManifest />-->
 </template>
 <script setup lang="ts">
+import {ref, onMounted, onUnmounted, computed} from 'vue-demi';
+const isOnline = ref(navigator.onLine);
+const onlineStatus = computed(() => (isOnline.value ? 'Online' : 'Offline')); //todo what nuxt provide to identify browser status
+const updateOnlineStatus = () => {
+  isOnline.value = navigator.onLine;
+};
+
+// Lifecycle hooks for component mount and unmount events, is this correct to use ?
+onMounted(() => {
+  window.addEventListener('online', updateOnlineStatus);
+  window.addEventListener('offline', updateOnlineStatus);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('online', updateOnlineStatus);
+  window.removeEventListener('offline', updateOnlineStatus);
+});
 </script>
